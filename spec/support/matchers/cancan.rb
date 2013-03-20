86a0e2df139140116115a6897a9fa6ab0e392788
Michael Boone <fuag155555@gmail.com>
2013-03-20 16:06:34 -0700

    matcher clean up

--- spec/support/matchers/cancan.rb
+++ spec/support/matchers/cancan.rb
@@ -4,23 +4,38 @@
 # @user.should have_ability({create: true, read: false, update: false, destroy: true}, for: Post.new)
 RSpec::Matchers.define :have_ability do |ability_hash, options = {}|
   match do |user|
-    ability         = Ability.new user
-    target          = options[:for]
+    ability = Ability.new user
+    target = options[:for]
     @ability_result = {}
-    ability_hash    = {ability_hash: true} if ability_hash.is_a? Symbol # e.g.: :create => {:create => true}
-    ability_hash    = ability_hash.inject({}){|_, i| _.merge({i: true}) } if ability_hash.is_a? Array # e.g.: [:create, :read] => {:create=>true, :read=>true}
+    
+    # :create -> { create: true }
+    ability_hash = { ability_hash: true } if ability_hash.is_a? Symbol 
+    
+     # e.g.: [:create, :read] -> {create: true, read: true}
+    ability_hash = ability_hash.inject({}) do | accumulator_hash , value | 
+      accumulator_hash.merge value: true
+    end if ability_hash.is_a? Array 
+    
     ability_hash.each do |action, true_or_false|
       @ability_result[action] = ability.can?(action, target)
     end
+
     !ability_hash.diff(@ability_result).any?
   end
 
   failure_message_for_should do |user|
-    ability_hash,options = expected
-    ability_hash         = {ability_hash: true} if ability_hash.is_a? Symbol # e.g.: :create
-    ability_hash         = ability_hash.inject({}){|_, i| _.merge({i: true}) } if ability_hash.is_a? Array # e.g.: [:create, :read] => {:create=>true, :read=>true}
-    target               = options[:for]
-    message              = "expected User:#{user} to have ability:#{ability_hash} for #{target}, but actual result is #{@ability_result}"
+    ability_hash, options = expected
+
+    # :create -> create: true
+    ability_hash = { ability_hash: true } if ability_hash.is_a? Symbol 
+    
+    # e.g.: [:create, :read] -> create: true, read: true
+    ability_hash = ability_hash.inject({}) do | accumulator_hash , value | 
+      accumulator_hash.merge value: true 
+    end if ability_hash.is_a? Array 
+
+    target = options[:for]
+    message = "expected User:#{user} to have ability:#{ability_hash} for #{target}, but actual result is #{@ability_result}"
   end
 
   #to clean up output of RSpec Documentation format
