aab74731982887c03de184721ac8a629d9c99f66
Michael Boone <fuag155555@gmail.com>
2013-05-10 01:01:27 -0700

    added default rails env

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# default the environment to produciton for railsplayground
+ENV['RAILS_ENV'] ||= 'production'
+
 # Initialize the rails application
 Mildwurm::Application.initialize!
