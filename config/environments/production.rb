753542459805ca28c00f2f791890300c05362e2c
Michael Boone <fuag155555@gmail.com>
2013-05-10 00:27:55 -0700

    temp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,15 @@ Mildwurm::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Rails playground stuff
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: "localhost",
+    port: 25,
+    domain: "mildwurmproductions.com",
+    authentication: :login,
+    user_name: "jason@mildwurmproductions.com",
+    password: "ppC441p5gS"
+  }
 end
